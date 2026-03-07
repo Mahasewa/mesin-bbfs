b2cfca8874013d64889f3158e766fac4e8726bf9
Mahasewa <alimuaziz@gmail.com>
2026-03-08 00:41:57 +0800

    Update aplikasi_web.py

--- aplikasi_web.py
+++ aplikasi_web.py
@@ -240,12 +240,13 @@ if tombol_proses and input_bbfs:
 
 # 3. Proses 2D
     if show_2d:
-        a2, b2, p2 = get_kombinasi(input_bbfs, 2, set())
-        a2_final = [a for a in a2 if not is_tereliminasi_2d(a, f_kep, f_ekor)]
-        data_hasil.extend(a2_final)
-        if b2: st.warning(f"⚠️ BERURUTAN (2D): {len(b2)} Line")
-        if p2: st.error(f"🔥 DATA PANAS (2D): {len(p2)} Line")
-
+            a2, b2, p2 = get_kombinasi(input_bbfs, 2, set())
+            a2_final = [a for a in a2 if not is_tereliminasi_2d(a, f_kep, f_ekor)]
+            data_hasil.extend(a2_final)
+            if b2:
+                st.warning(f"BERURUTAN (2D): {len(b2)} Line -> {', '.join(b2)}")
+            if p2:
+                st.error(f"DATA PANAS (2D): {len(p2)} Line -> {', '.join(p2)}")
  
  # 4. Proses Kembar (Strict)
     if show_twin:
@@ -274,6 +275,7 @@ elif tombol_proses and not input_bbfs:
     st.error("Isi angkanya dulu Koh!")
 
 st.markdown("<p style='text-align: center; font-size: 0.8rem; color: #888;'>© 2026 Mahasewa BBFS Digital Team</p>", unsafe_allow_html=True)
+
 
 
 
